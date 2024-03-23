f15cebdf0ba0606b044eadd12c14d9257e5f3eb3
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-03-23 17:25:09 +0530

    feat : adding delete mapping for delete student by id

--- src/main/java/com/shubh/student_management/controller/StudentController.java
+++ src/main/java/com/shubh/student_management/controller/StudentController.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +16,7 @@ import com.shubh.student_management.entity.Student;
 import com.shubh.student_management.service.StudentService;
 
 @RestController
+@CrossOrigin()
 public class StudentController {
 
     private StudentService studentService;
@@ -23,17 +27,20 @@ public class StudentController {
 
     @GetMapping("/students")
     public ResponseEntity<List<Student>> getAllStudents(){
-        return ResponseEntity.ok().body(studentService.getAllStudents());
+        return ResponseEntity.ok(studentService.getAllStudents());
     }
 
     @PostMapping("/add-student")
     public ResponseEntity<Student> addStudent(@RequestBody Student newStudent){
-        String name = newStudent.getLastName();
-        System.out.println(name);
         Student student = new Student(newStudent.getFirstName(),
         newStudent.getLastName(),
         newStudent.getEmail());
         
         return ResponseEntity.status(HttpStatus.CREATED).body(studentService.addStudent(student));
+    }
+
+    @DeleteMapping("/delete-student/{studentId}")
+    public void deleteStudentById(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
     }
 }
